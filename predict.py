ad188758e08b5ca42e79e17a82842baaecb97c62
nandehu <CoralGift.N.H@uec.ac.jp>
2017-11-14 18:58:28 +0900

    change caffe to pytesseract

--- predict.py
+++ predict.py
@@ -1,26 +1,37 @@
-import numpy as np
-import os
-import sys
+# import the necessary packages
+from PIL import Image
+import pytesseract
 import argparse
-import glob
-import time
-
-import caffe
+import cv2
+import os
 
 def predict(file):
-    model_def = "./data/mnist/lenet.prototxt"
-    pretrained_model = "./data/mnist/lenet_iter_10000.caffemodel"
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True,
+    	help="path to input image to be OCR'd")
+    ap.add_argument("-p", "--preprocess", type=str, default="thresh",
+    	help="type of preprocessing to be done")
+    args = vars(ap.parse_args())
 
-    caffe.set_mode_gpu()
+    # check to see if we should apply thresholding to preprocess the
+    # image
+    if args["preprocess"] == "thresh":
+    	gray = cv2.threshold(file, 0, 255,
+    		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
-    # Make classifier.
-    classifier = caffe.Classifier(model_def, pretrained_model)
+    # make a check to see if median blurring should be done to remove
+    # noise
+    elif args["preprocess"] == "blur":
+    	gray = cv2.medianBlur(file, 3)
 
-    # Load numpy array (.npy), directory glob (*.jpg), or image file.
-    #grayimg = caffe.io.load_image(file, color=False)[:,:,0]
-    inputs = [np.reshape(file, (28, 28, 1))]
+    # write the grayscale image to disk as a temporary file so we can
+    # apply OCR to it
+    filename = "{}.png".format(os.getpid())
+    cv2.imwrite(filename, gray)
 
-    # Classify.
-    start = time.time()
-    predictions = classifier.predict(inputs)
-    print(predictions)
+    # load the image as a PIL/Pillow image, apply OCR, and then delete
+    # the temporary file
+    text = pytesseract.image_to_string(Image.open(filename))
+    os.remove(filename)
+    print(text)
