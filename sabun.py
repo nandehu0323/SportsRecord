74e51acc7881e34b1cb7c5e0a8b903637ee19aa6
鎌田峻輔 <s.kamada@s-kamada.local>
2017-11-18 22:18:41 +0900

    remove some small recrtangle

--- sabun.py
+++ sabun.py
@@ -12,12 +12,12 @@ WINDOW_CUTBK = "Cut background"
 
 FILE_ORG = "ball2-1.mov"
 
-#p0に最も近い点を求める関数search_neighbor
-def search_neighbor(p0,ps):
+def search_neighbor(p0, ps):
     L = np.array([])
     for i in range(ps.shape[0]):
-        L = np.append(L,np.linalg.norm(ps[i])-p0)
-    return np.argmin(L),min(L) #返り値は近傍点の(ラベル番号),(距離)
+        L = np.append(L,np.linalg.norm(ps[i]-p0))
+        L[np.where(L==0)] = 999 #値が0の要素を無理やり書き換える
+    return np.argmin(L),L[np.argmin(L)] #返り値は近傍点の(ラベル番号),(距離)
 
 #ウィンドウ命名
 cv2.namedWindow(WINDOW_SRC)
@@ -36,8 +36,7 @@ back_frame = np.zeros_like(i_frame, np.float32)
 time = 0 #経過時間(フレーム)
 while has_next == True:
 
-    time +=1
-    # print(time)
+    time += 1
     # 入力画像を浮動小数点型に変換
     f_frame = i_frame.astype(np.float32)
 
@@ -53,7 +52,7 @@ while has_next == True:
                                 max_pixel,
                                 cv2.THRESH_BINARY)
 
-    stats = centroids = near_index = near_dis = []
+    stats = centroids = near_index = near_dist = []
 
     # ラベリングのため深度変更
     dst_frame = np.uint8(dst_frame)
@@ -61,23 +60,30 @@ while has_next == True:
     if time>=50:
         # ラベリング処理
         nlabels, labeledimg, stats, CoGs = cv2.connectedComponentsWithStats(dst_frame)
-
+        region = np.empty((0,5),int)
+        center = np.empty((0,2),int)
 
         for n in range(nlabels):
-            x,y,width,height,size = stats[n]
-            near_index, near_dis = search_neighbor(CoGs[n],CoGs)
-            if size<30:
-                nlabels -= 1
-                stats.delete()
-                CoGs.delete()
+            x, y, width, height, size = stats[n]
+
+            if size>30 and size<1000:
+                #十分な大きさの領域のみ抽出
+                region = np.vstack((region,stats[n]))
+                center = np.vstack((center,CoGs[n]))
+
+        for i in range(len(region)):
+            x, y, width, height, size = region[i]
+            near_index, near_dist = search_neighbor(center[i],center)
+            if time == 50:
+                print("label" + str(i) + "\n near_index: " + str(near_index) + " near_dist: " + str(near_dist))
+            if near_dist <= 50:
+                c1x = min(x        , region[near_index,0])
+                c1y = min(y        , region[near_index,1])
+                c2x = max(x+width  , region[near_index,0]+region[near_index,2])
+                c2y = max(y+height , region[near_index,1]+region[near_index,3])
+                cv2.rectangle(dst_frame, (c1x,c1y), (c2x,c2y), (255,255,255), 2)
             else:
-                #cv2.rectangle(dst_frame, (x-2, y-2), (x+width+2, y+height+2), (255,255,255), 2)
-                cv2.rectangle(diff_frame, (x-2, y-2), (x+width+2, y+height+2), (255,255,255), 2)
-
-                if time == 70:
-                #print("[time=70,point" + str(n) + "]\n)
-                    print("[time=70,point" + str(n) + "]\n near_index: " + str(near_index) + " near_dis: " + str(near_index) )
-
+                cv2.rectangle(dst_frame, (x-2, y-2), (x+width+2, y+height+2), (255,255,255), 2)
 
     # 背景の更新
     cv2.accumulateWeighted(f_frame, back_frame, 0.025)
